0fd96f72b5c82ed403783fe9e96b0c4ec51dfb9d
Viral <patel.viral@northeastern.edu>
2020-10-07 20:32:56 -0400

    Home page update (#3)
    * Updated background image
    
    * Added image link
    
    * Updated home page - design and content

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,10 +1,56 @@
 import React, {Fragment} from "react";
-import "./home-style.css";
-import vp_profile_image from "./vp-profile-image.jpg";
+
 import AnchorTag from "../anchor-tag";
 
+import "./home-style.css";
+import vp_profile_image from "./vp-profile-image.jpg";
+
+const getDataForScreenSizeGreaterThanSmall = () => {
+    return (
+        <div className="d-none d-md-block">
+            <div className="row mb-4">
+                <div className="d-flex justify-content-center col-lg-4 offset-lg-4 col-10 offset-1">
+                    <span className="vp-home-my-name">Hey there !</span>
+                </div>
+            </div>
+            <div className="row mb-4">
+                <div className="d-flex justify-content-center col-lg-4 offset-lg-4 col-10 offset-1">
+                    <span className="vp-home-my-name">I'm Viral Patel</span>
+                </div>
+            </div>
+            <div className="row mb-4">
+                <div className="d-flex justify-content-center col-lg-6 offset-lg-3 col-12">
+                    <span className="vp-home-my-position">Software Developer & Designer</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+const getDataForScreenSizeSmallerThanMedium = () => {
+    return (
+        <div className="d-bock d-md-none">
+            <div className="row mb-4">
+                <div className="d-flex justify-content-center col-10 offset-1">
+                    <span className="vp-home-my-name-small">Hey there !</span>
+                </div>
+            </div>
+            <div className="row mb-4">
+                <div className="d-flex justify-content-center col-10 offset-1">
+                    <span className="vp-home-my-name-small">I'm Viral Patel</span>
+                </div>
+            </div>
+            <div className="row mb-4">
+                <div className="d-flex justify-content-center col-12">
+                    <span className="vp-home-my-position-small">Software Developer & Designer</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 const Home = () => {
-    const social_links = [
+    /*const social_links = [
         {
             "css_class": "mui-btn mui-btn--fab mui-btn--primary",
             "href": "https://www.linkedin.com/in/patelviralb",
@@ -29,9 +75,10 @@ const Home = () => {
             "target": "_blank",
             "icon_class": "fab fa-twitter fa-lg",
         },
-    ];
+    ];*/
+
     const resume_details = {
-        "css_class": "btn btn-success font-weight-bold shadow rounded",
+        "css_class": "btn btn-success font-weight-bold shadow rounded text-decoration-none",
         "href": "https://github.com/patelviralb/Resume/raw/master/Viral-Patel_Resume.pdf",
         "download": "Viral-Patel_Resume.pdf",
         "target": "_blank",
@@ -41,38 +88,25 @@ const Home = () => {
 
     return (
         <Fragment>
-            <a id="vp-home"></a>
+            <a id="vp-home"/>
             <div className="vp-home-background-image">
                 <div className="vp-home-content">
                     <div className="container">
                         <div className="row mb-4">
                             <div className="d-flex justify-content-center col-md-4 offset-md-4 col-6 offset-3">
-                                <img className="rounded-circle w-100 h-100" src={vp_profile_image}/>
+                                <img className="rounded-circle w-100 h-100" src={vp_profile_image} alt="Hey there ! I'm Viral Patel"/>
                             </div>
                         </div>
-                        <div className="row mb-4">
-                            <div className="d-flex justify-content-center col-lg-4 offset-lg-4 col-10 offset-1">
-                                <h2 className="vp-home-my-name text-white font-weight-bold">Viral Patel</h2>
-                            </div>
-                        </div>
-                        <div className="row mb-4">
-                            <div className="d-flex justify-content-center col-lg-4 offset-lg-4 col-12">
-                                <h4 className="vp-home-my-position text-white font-weight-bold">Software Developer</h4>
-                            </div>
-                        </div>
-                        <div className="row mb-4">
-                            <div className="d-flex justify-content-center col-lg-4 offset-lg-4 col-12">
-                                <h4 className="vp-home-my-education text-white font-weight-bold">
-                                    MS - Computer Science
-                                </h4>
-                            </div>
-                        </div>
+
+                        {getDataForScreenSizeGreaterThanSmall()}
+                        {getDataForScreenSizeSmallerThanMedium()}
+
                         <div className="row mb-4">
                             <div className="d-flex justify-content-center col-md-6 offset-md-3 col-12">
                                 <AnchorTag params={resume_details}/>
                             </div>
                         </div>
-                        <div className="row mb-4">
+                        {/*<div className="row mb-4">
                             <div className="d-flex justify-content-center col-md-6 offset-md-3 col-12">
                                 {
                                     social_links.map(eachObject => {
@@ -82,7 +116,7 @@ const Home = () => {
                                     })
                                 }
                             </div>
-                        </div>
+                        </div>*/}
                     </div>
                 </div>
             </div>

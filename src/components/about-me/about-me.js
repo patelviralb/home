653453e1abbe069f1c83a396f3d3767206c27a5f
patelviralb <vpatel@quickbase.com>
2020-10-07 23:23:30 -0400

    Updated about me style and design

--- src/components/about-me/about-me.js
+++ src/components/about-me/about-me.js
@@ -1,47 +1,47 @@
 import React, {Fragment} from "react";
 
 import "./about-me-style.css";
-import AboutMeParagraph from "./about-me-paragraph";
 import PageBreak from "../page-break";
 
 const AboutMe = () => {
-    const about_me_content = [
-        "Hello, I'm Viral Patel! Currently I am a Computer Science Graduate Student at Northeastern University, Boston.",
-
-        "Before commencing a graduate degree, I was working for Infosys Ltd. and Prolifics in India since 2014. " +
-        "My 5 years of work experience working with EDI and gateway team has helped me grow my skills in team " +
-        "collaboration, verbal communication, and project leadership. Along with these skills, I also have domain " +
-        "knowledge of the healthcare insurance industry that I gained from projects I worked on.",
-
-        "I am interested in learning and working on Cloud Computing. The change of perspective in designing " +
-        "applications and systems from central architecture to purely cloud-based distributed architecture is what " +
-        "excites me. Along with Cloud Computing, I am also interested in the field of Artificial intelligence and " +
-        "Machine Learning Applications along with web technologies and how I can put all these pieces together to " +
-        "create software systems or product which can help people or an organization in their day-to-day life.",
-
-        "When I am not working, you'll find me casually reading books, surfing articles on the internet or watching " +
-        "some cartoons, sci-fi series or movies and I like to call it my passive learning.",
-    ];
-
     return (
         <Fragment>
-            <a id="vp-about-me"></a>
-            <div className="mt-5">
-                <div className="container">
-                    <div className="row mt-5 mb-5">
-                        <div className="col-12">
-                            <h1 className="text-justify vp-about-me-heading">
-                                About
-                            </h1>
+            <a id="vp-about-me"/>
+            <div className="container-fluid mt-5 mt-md-n5">
+                <div className="row">
+                    <div className="col-md-10 offset-md-1 col-12">
+                        <div className="card shadow-lg rounded mt-md-n5">
+                            <div className="card-body text-justify">
+                                <p className="mb-5">
+                                    <span className="vp-about-me-content">
+                                        I'm Software Engineer learning to make things simple and better. Currently I'm
+                                        Computer Science Graduate Student at Northeastern University, Boston.
+                                    </span>
+                                </p>
+                                <p className="mb-5">
+                                    <span className="vp-about-me-content">
+                                        Before commencing my graduate degree, I was working as a Software Developer
+                                        with nearly 5 years of work experience. Creating a high quality modular code
+                                        which is easier to maintain and extend was one of my considerable
+                                        accomplishments. I also like to continuously challenge myself to innovate and/or
+                                        automate things which helps me and my team to create high quality software
+                                        applications. Being a firm believer of life-long learning, I always try to
+                                        learn through experimentation. As a result, I would often work on some
+                                        personal projects which includes domains like Web Technologies and Cloud
+                                        Computing. Apart from these, I am also interested in applied Machine Learning
+                                        and Artificial Intelligence.
+                                    </span>
+                                </p>
+                                <p className="mb-5">
+                                    <span className="vp-about-me-content">
+                                        During my time-off, you'll find me casually reading books, surfing articles on
+                                        the internet or watching some cartoons, sci-fi series or movies and I like to
+                                        call it my passive learning.
+                                    </span>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                    {
-                        about_me_content.map(eachParagraph => {
-                            return (
-                                <AboutMeParagraph content={eachParagraph}/>
-                            );
-                        })
-                    }
                 </div>
             </div>
             <PageBreak/>

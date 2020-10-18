b6a030c0ad8b121f5bb8f4d9d65be60951d6faf6
patelviralb <patel.viral@northeastern.edu>
2020-10-18 02:14:49 -0400

    Updated Navbar color visibility events

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
     const navbarLinks = {
         "Home": "#vp-home",
         "About": "#vp-about-me",
-        /*"Profiles": "#vp-my-social-profiles",
         "Technical Knowledge": "#vp-my-technical-knowledge",
+        /*"Profiles": "#vp-my-social-profiles",
         "Work Experience": "#vp-my-work-experience",
         "Education": "#vp-my-education",
         "Testimonies": "#vp-my-testimonies",
@@ -16,22 +16,34 @@ const Navbar = () => {
 
     const [bgColor, setBgColor] = useState("");
 
-    const listenScrollEvent = e => {
-        if (window.scrollY > 500) {
-            setBgColor("bg-primary")
-        } else {
-            setBgColor("")
-        }
-    };
-
     React.useEffect(() => {
+        const listenScrollEvent = () => {
+            HandleNavbarColorChangeEvents();
+        };
+
+        const listenResizeEvent = () => {
+            HandleNavbarColorChangeEvents();
+        }
+
+        const HandleNavbarColorChangeEvents = () => {
+            const navbarExpansionButton = document.getElementById('vp-navbar-expand-button');
+            const navbarButtonVisibility = window.getComputedStyle(navbarExpansionButton).display;
+            
+            if (window.pageYOffset > 500 || navbarButtonVisibility !== "none") {
+                setBgColor("bg-primary")
+            } else {
+                setBgColor("")
+            }
+        }
+
         window.addEventListener('scroll', listenScrollEvent);
+        window.addEventListener('resize', listenResizeEvent);
     }, []);
 
     return (
         <nav className={`navbar navbar-expand-lg navbar-primary fixed-top ${bgColor}`}>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
-                <i className="fas fa-bars text-white"/>
+            <button id="vp-navbar-expand-button" className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
+                <i id="vp-navbar-button-icon" className="fas fa-bars text-white"/>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul className="navbar-nav">

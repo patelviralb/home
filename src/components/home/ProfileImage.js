504edc438e9c57b18c9e7112cd3761999ca197dc
Viral <patel.viral@northeastern.edu>
2020-11-01 22:42:10 -0500

    Profile Image style update

--- src/components/home/ProfileImage.js
+++ src/components/home/ProfileImage.js
@@ -1,21 +1,20 @@
 import React from "react";
 
 import vp_profile_image from "./vp-profile-image.jpg";
-import {CenterAlignedContainer} from "../styled/home";
+import { CenterAlignedContainer } from "../styled/home";
 
 const ProfileImage = () => {
-    return (
-        <div className="row">
-            <CenterAlignedContainer
-                className="col-6 offset-3 col-md-4 offset-md-4">
-                <img
-                    className="rounded-circle"
-                    src={vp_profile_image}
-                    alt={`Hey there ! I'm Viral Patel`}
-                />
-            </CenterAlignedContainer>
-        </div>
-    );
+  return (
+    <div className="row">
+      <CenterAlignedContainer className="col-6 offset-3 col-md-4 offset-md-4">
+        <img
+          className="rounded-circle w-100 h-100"
+          src={vp_profile_image}
+          alt={`Hey there ! I'm Viral Patel`}
+        />
+      </CenterAlignedContainer>
+    </div>
+  );
 };
 
 export default ProfileImage;

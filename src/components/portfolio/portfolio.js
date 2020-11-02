ba7bffaa563597ad3f945fbbfcdd020425d31b80
Viral <patel.viral@northeastern.edu>
2020-11-01 21:27:14 -0500

    Moved navbar to containers

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,21 +1,21 @@
-import React, {Fragment} from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React, { Fragment } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import Navbar from "../navbar";
+import Navbar from "../../containers/navbar";
 import Home from "../home";
 import AboutMe from "../about-me";
 import TechnicalKnowledge from "../technical-knowledge";
 
 const Portfolio = () => {
-    return (
-        <Fragment>
-            <Navbar/>
-            <Home/>
-            <AboutMe/>
-            <TechnicalKnowledge/>
-        </Fragment>
-    );
-}
+  return (
+    <Fragment>
+      <Navbar />
+      <Home />
+      <AboutMe />
+      <TechnicalKnowledge />
+    </Fragment>
+  );
+};
 
 export default Portfolio;

33a3ad6c524ca1212400bdd1f65eea9e9db9645e
Viral <patel.viral@northeastern.edu>
2021-03-31 16:45:25 -0400

    Code reformat for languages

--- src/components/technical-knowledge/languages/technical-knowledge-languages.js
+++ src/components/technical-knowledge/languages/technical-knowledge-languages.js
@@ -1,67 +1,85 @@
 import React from "react";
 
+import languageData from "../../../data/technical-knowledge/languages.json";
+
 import {
-    CppSvg,
-    CSvg,
-    JavaSvg,
-    PythonSvg,
+  CppSvg,
+  CSvg,
+  JavaSvg,
+  PythonSvg,
+  ScalaSvg,
 } from "../svg/technical-knowledge-svg";
-import {Proficiency} from "../../styles/technical-knowledge";
+import { Proficiency } from "../../styles/technical-knowledge";
 
-const createLanguageCard = (language) => {
-    let languageProficiency;
-    let languageSvgComponent;
+const getSvg = (languageName) => {
+  switch (languageName) {
+    case "Java":
+      return <JavaSvg />;
+    case "C":
+      return <CSvg />;
+    case "C++":
+      return <CppSvg />;
+    case "Python":
+      return <PythonSvg />;
+    case "Scala":
+      return <ScalaSvg />;
+    default:
+      return null;
+  }
+};
 
-    if (language === "Java") {
-        languageProficiency = "Intermediate";
-        languageSvgComponent = <JavaSvg/>;
-    } else if (language === "C") {
-        languageProficiency = "Intermediate";
-        languageSvgComponent = <CSvg/>;
-    } else if (language === "C++") {
-        languageProficiency = "Novice";
-        languageSvgComponent = <CppSvg/>;
-    } else if (language === "Python") {
-        languageProficiency = "Beginner";
-        languageSvgComponent = <PythonSvg/>;
-    }
+const getCssClasses = (index) => {
+  let cssClasses = "col-12 col-md-6 col-lg-3";
+  switch (index) {
+    case 0:
+      cssClasses += "";
+      break;
+    case 1:
+      cssClasses += " mt-md-0 mt-4";
+      break;
+    case 2:
+      cssClasses += " mt-lg-0 mt-4";
+      break;
+    case 3:
+      cssClasses += " mt-lg-0 mt-4";
+      break;
+    default:
+      cssClasses += " mt-4";
+      break;
+  }
 
-    return (
-        <div className="card-body text-justify">
-            <h5>
-                {languageSvgComponent}
-                <span className="ml-2">{language}</span>
-            </h5>
-            <Proficiency>Proficiency: {languageProficiency}</Proficiency>
-        </div>
-    );
+  return cssClasses;
 };
 
 const TechnicalKnowledgeLanguages = () => {
-    return (
-        <div className="container-fluid mt-4">
-            <div className="row">
-                <div className="col-12 col-md-6 col-lg-3">
-                    <div className="card shadow rounded">
-                        {createLanguageCard("Java")}
+  console.log(languageData.languages);
+  return (
+    <div className="container-fluid mt-4">
+      <div className="row">
+        {languageData.languages
+          ? languageData.languages.map((language, index) => {
+              const cssClasses = getCssClasses(index);
+
+              return (
+                <div className={cssClasses} key={index}>
+                  <div className="card shadow rounded">
+                    <div className="card-body text-justify">
+                      <h5>
+                        {getSvg(language.name)}
+                        <span className="ml-2">{language.name}</span>
+                      </h5>
+                      <Proficiency>
+                        Proficiency: {language.proficiency}
+                      </Proficiency>
                     </div>
+                  </div>
                 </div>
-                <div className="col-12 col-md-6 col-lg-3 mt-md-0 mt-4">
-                    <div className="card shadow rounded">{createLanguageCard(
-                        "C")}</div>
-                </div>
-                <div className="col-12 col-md-6 col-lg-3 mt-4 mt-lg-0">
-                    <div className="card shadow rounded">{createLanguageCard(
-                        "C++")}</div>
-                </div>
-                <div className="col-12 col-md-6 col-lg-3 mt-4 mt-lg-0">
-                    <div className="card shadow rounded">
-                        {createLanguageCard("Python")}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+              );
+            })
+          : null}
+      </div>
+    </div>
+  );
 };
 
 export default TechnicalKnowledgeLanguages;

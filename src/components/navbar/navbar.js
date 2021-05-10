1993f58852beddcfda05f55b2cb58801aceb8a9d
Viral <patel.viral@northeastern.edu>
2021-05-10 17:29:03 -0400

    Navbar refactoring

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,47 +1,25 @@
 import React, { useState } from "react";
 
+import navbarLinks from "../../data/navbar/navbar-links.json";
 import Navlink from "./navlink";
 
-const Navbar = () => {
-  const navbarLinks = [
-    {
-      name: "Home",
-      link: "#vp-home",
-    },
-    {
-      name: "About",
-      link: "#vp-about-me",
-    },
-    {
-      name: "Technical Knowledge",
-      link: "#vp-my-technical-knowledge",
-    },
-    {
-      name: "Work Experience",
-      link: "#vp-my-work-experience",
-    },
-    /* {
-      name: "Education",
-      link: "#vp-my-education",
-    },
-    {
-      name: "Testimonies",
-      link: "#vp-my-testimonies",
-    },
-    {
-      name: "Projects",
-      link: "#vp-my-projects",
-    }, */
-    {
-      name: "Contact Me",
-      link: "#vp-contact-me",
-    },
-    /* {
-      name: "Profiles",
-      link: "#vp-my-social-profiles",
-    }, */
-  ];
+// {
+//   name: "Education",
+//   link: "#vp-my-education",
+// },
+// {
+//   name: "Testimonies",
+//   link: "#vp-my-testimonies",
+// },
+// {
+//   name: "Projects",
+//   link: "#vp-my-projects",
+// },{
+//   name: "Profiles",
+//   link: "#vp-my-social-profiles",
+// },
 
+const Navbar = () => {
   const [bgColor, setBgColor] = useState("");
 
   React.useEffect(() => {
@@ -93,7 +71,7 @@ const Navbar = () => {
       </button>
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
-          {navbarLinks.map((section, index) => {
+          {navbarLinks.links.map((section, index) => {
             return (
               <Navlink key={index} name={section.name} href={section.link} />
             );

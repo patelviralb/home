c5a0bfc3cc0a8fc8df7fc569dc37f59ebdde7914
Viral <patel.viral@northeastern.edu>
2021-05-12 19:20:51 -0400

    After reset manual merging

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import Portfolio from "./components/portfolio";
+
+function App() {
+    return (
+        <Portfolio/>
+    );
+}
+
+export default App;
